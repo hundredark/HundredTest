0af9ced322549d7952496a780e0883cbce330584
hundredark <hundredark@gmail.com>
2023-02-06 18:07:46 +0800

    Test os

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <RouterView />
   </div>
   <div>
-    <div>test</div>
+    <div>{{ text }}</div>
     <div><button class="test-btn">click</button></div>
     <input class="border-black border-2" type="text" />
   </div>
@@ -15,6 +15,18 @@ import { onBeforeUnmount } from 'vue';
 import { RouterView } from 'vue-router';
 import ClipboardJS from 'clipboard';
 import Header from './components/Header.vue';
+
+const environment = () => {
+  if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.MixinContext) {
+    return 'iOS';
+  }
+  if (window.MixinContext && (typeof window.MixinContext.getContext === 'function')) {
+    var ctx = JSON.parse(window.MixinContext.getContext());
+    return ctx.platform || 'Android';
+  }
+  return undefined;
+};
+const text = environment();
 
 const clipboard = new ClipboardJS('.test-btn', {
   text: () => "test test"
